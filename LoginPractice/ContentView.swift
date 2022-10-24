5316c64e56c008120a08b2671ca0e28946341102
Ashin <ashinwangboy@gmail.com>
2022-10-24 16:23:24 +0800

    Initial Commit

---
+++ LoginPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginPractice
+//
+//  Created by Ashin Wang on 2022/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginPractice/LoginPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPracticeApp.swift
+//  LoginPractice
+//
+//  Created by Ashin Wang on 2022/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
